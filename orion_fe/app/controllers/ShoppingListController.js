3b2b0a9d2116ecc6268c83a6ea7a281a236dfaa0
Mårten Dahlquist <marten@dahlquist.info>
2015-10-26 21:56:52 +0100

    Finalize post method

--- orion_fe/app/controllers/ShoppingListController.js
+++ orion_fe/app/controllers/ShoppingListController.js
@@ -14,7 +14,7 @@
 		 	mainService.insertItem(typeUrl,function(data){		  
 			},$scope.list);
 	}
-    $scope.statuses = [{name:'Aktiv',value:'active'},{name:'Inaktiv',value:'inactive'}];
+    $scope.statuses = [{name:'Aktiv',value:'1'},{name:'Inaktiv',value:'0'}];
 });
 
 //data för rad 7 och 12 kommer att ha olika innehåll, därför två metoder.
--- orion_fe/app/services/mainService.js
+++ orion_fe/app/services/mainService.js
@@ -1,16 +1,15 @@
 ﻿app.factory('mainService', function($http) {
 	var urlBase = 'http://localhost:8080';
-	var userId = '2'; //TODO
+	var token = '1224'; //TODO
 	   return {
 	     getItems: function(referer,callback) {
-	       $http.get(urlBase+'/'+referer+'/'+userId).success(callback);
+	       $http.get(urlBase+'/'+referer+'/'+token).success(callback);
 	     },
 	     getItem: function(referer,id,callback) {
-		   $http.get(urlBase+'/'+referer+'/'+userId+'/'+id).success(callback);
+		   $http.get(urlBase+'/'+referer+'/'+token+'/'+id).success(callback);
 		  },
 		  insertItem: function (referer,callback, postjsonobj) {
-			  console.log(postjsonobj);
-	          $http.post(urlBase+'/'+referer+'/create/'+userId, postjsonobj);
+	          $http.post(urlBase+'/'+referer+'/create/'+token, postjsonobj);
 	      }
 	   }
 });
